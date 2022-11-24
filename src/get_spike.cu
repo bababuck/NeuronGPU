92f67c364373dd67df2a0aa73f5e1060e63b636d
bababuck <buchner.ryan@gmail.com>
2022-11-24 01:05:15 -0800

    Add device GetSpikes function

--- src/get_spike.cu
+++ src/get_spike.cu
@@ -154,6 +154,34 @@ __global__ void GetSpikes(double *spike_array, int array_size, int n_port,
   }
 }
 
+__device__ void GetSpikes(double *spike_array, int array_size, int n_port,
+			  int n_var,
+			  float *port_weight_arr,
+			  int port_weight_arr_step,
+			  int port_weight_port_step,
+			  float *port_input_arr,
+			  int port_input_arr_step,
+			  int port_input_port_step)
+{
+  for (int i=threadIdx.x;i<array_size * n_port;i+=blockDim.x) {
+    int i_target = i % array_size;
+    int port = i / array_size;
+
+    if (i_target < array_size && port<n_port) {
+      int i_array = port*array_size + i_target;
+      int port_input = i_target*port_input_arr_step
+        + port_input_port_step*port;
+      int port_weight = i_target*port_weight_arr_step
+        + port_weight_port_step*port;
+      double d_val = (double)port_input_arr[port_input]
+        + spike_array[i_array]
+        * port_weight_arr[port_weight];
+    
+      port_input_arr[port_input] = (float)d_val;
+    }
+  }
+}
+
 __device__
 int NeuronGPU::ClearGetSpikeArrays()
 {
